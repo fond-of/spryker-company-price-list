a093f9c247c5c49c03516ea9125466945fca6d2b
Daniel Rose <daniel-rose@gmx.de>
2021-11-04 13:40:26 +0100

    Add search price list query expander plugins

---
+++ src/FondOfSpryker/Shared/CompanyPriceList/CompanyPriceListConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FondOfSpryker\Shared\CompanyPriceList;
+
+interface CompanyPriceListConstants
+{
+    /**
+     * @var string
+     */
+    public const FILTER_FIELD_TYPE_ID_CUSTOMER = 'idCustomer';
+
+    /**
+     * @var string
+     */
+    public const FILTER_FIELD_TYPE_ID_COMPANY = 'idCompany';
+}
